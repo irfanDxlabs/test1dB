fe1527f85bc54b042d8043dc828ddb7b400e4815
Irfan Shaik <irfan.shaik@dxlabs.com.au>
2021-11-29 17:15:04 +1100

    added a new table - Persons

---
+++ Tables/dbo.Persons.sql
@@ -0,0 +1,9 @@
+CREATE TABLE [dbo].[Persons]
+(
+[PersonID] [int] NULL,
+[LastName] [varchar] (255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[FirstName] [varchar] (255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[Address] [varchar] (255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[City] [varchar] (255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+) ON [PRIMARY]
+GO
