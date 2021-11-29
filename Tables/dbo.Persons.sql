aeff5d12c49b3e1327e929949edc01fc189841ba
Irfan Shaik <irfan.shaik@dxlabs.com.au>
2021-11-29 17:54:02 +1100

    added email1 coliumns

--- Tables/dbo.Persons.sql
+++ Tables/dbo.Persons.sql
@@ -5,6 +5,7 @@ CREATE TABLE [dbo].[Persons]
 [FirstName] [varchar] (255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [Address] [varchar] (255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [City] [varchar] (255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[Email] [varchar] (255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+[Email] [varchar] (255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[Email1] [varchar] (255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
