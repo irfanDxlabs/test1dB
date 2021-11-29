e6b5801f1abff773f46cb50d1db2366aa3f5332e
Irfan Shaik <irfan.shaik@dxlabs.com.au>
2021-11-29 17:27:16 +1100

    added a "Email" to Persons table

--- Tables/dbo.Persons.sql
+++ Tables/dbo.Persons.sql
@@ -4,6 +4,7 @@ CREATE TABLE [dbo].[Persons]
 [LastName] [varchar] (255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [FirstName] [varchar] (255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [Address] [varchar] (255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[City] [varchar] (255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+[City] [varchar] (255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[Email] [varchar] (255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
